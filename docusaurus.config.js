6fd2b8b4689071906a79a42395a68758e357a1dc
Ronak Raithatha <ronaksraithatha@gmail.com>
2021-01-24 16:59:10 -0500

    Initial commit

---
+++ docusaurus.config.js
@@ -0,0 +1,74 @@
+module.exports = {
+  title: 'Think Python - Abridged',
+  tagline: 'Abridged Adaptation of "Think Python by Allen Downey"',
+  url: 'https://your-docusaurus-test-site.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'ccsms-teals', // Usually your GitHub org/user name.
+  projectName: 'teals-associated-reading', // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: 'TEALS Associated Reading',
+      // logo: {
+      //   alt: 'My Site Logo',
+      //   src: 'img/logo.svg',
+      // },
+      items: [
+        {
+          to: 'docs/',
+          activeBasePath: 'docs',
+          label: 'Readings',
+          position: 'left',
+        },
+        // {
+        //   href: 'https://github.com/facebook/docusaurus',
+        //   label: 'GitHub',
+        //   position: 'right',
+        // },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      // links: [
+      //   {
+      //     title: 'Docs',
+      //     items: [
+      //       {
+      //         label: 'Style Guide',
+      //         to: 'docs/',
+      //       },
+      //       {
+      //         label: 'Second Doc',
+      //         to: 'docs/doc2/',
+      //       },
+      //     ],
+      //   }
+      // ],
+      copyright: `Copyright © ${new Date().getFullYear()} CCSMS - TEALS. Built with Docusaurus.`,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
