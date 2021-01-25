72a62ffcb358f2d79fb25e400b43dc119c3cf843
Ronak Raithatha <ronaksraithatha@gmail.com>
2021-01-24 20:49:14 -0500

    Deploy website - based on 3eac3650e5aac53ac3c5dc44137de5aee2c47b82

--- 935f2afb.34888a69.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{73:function(e){e.exports=JSON.parse('{"pluginId":"default","version":"current","label":"Next","isLast":true,"docsSidebars":{"someSidebar":[{"type":"link","label":"Unit 1 - Introduction to Python","href":"/docs/"}]},"permalinkToSidebar":{"/docs/":"someSidebar"}}')}}]);
---
+++ 935f2afb.f12227e6.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{73:function(e){e.exports=JSON.parse('{"pluginId":"default","version":"current","label":"Next","isLast":true,"docsSidebars":{"someSidebar":[{"type":"link","label":"Unit 1 - Introduction to Python","href":"teals-associated-reading/docs/"}]},"permalinkToSidebar":{"teals-associated-reading/docs/":"someSidebar"}}')}}]);
